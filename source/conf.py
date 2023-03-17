549f76c08ac8beba3cadf860510560c9297c2aff
SWHL <liekkaskono@163.com>
2023-03-17 09:49:54 +0800

    Update files

--- source/conf.py
+++ source/conf.py
@@ -10,6 +10,7 @@ project = 'Test'
 copyright = '2023,'
 author = 'Test'
 release = 'v2.1.3'
+repo_url = 'www.baidu.com'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -44,7 +45,6 @@ html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'analytics_anonymize_ip': False,
     'logo_only': True,
-    "logo_url": "https://readthedocs.org/",
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
@@ -53,13 +53,16 @@ html_theme_options = {
     'navigation_depth': 4,
     'includehidden': True,
     'titles_only': False,
-    'header_links': "Top 1|http://example.com/one, Top 2|http://example.com/two",
-    'footer_links': ",".join([
-        "About Us|http://example.com/",
-        "Contact|http://example.com/contact",
-        "Legal|http://example.com/dev/null",
-    ]),
 }
 
 html_static_path = ['_static']
 html_logo = "./_static/logo.png"
+
+SET_LOGO_URL_JS = f"""
+let div_logo = document.getElementsByClassName("wy-side-nav-search")[0];
+let a_logo = div_logo.getElementsByTagName("a");
+a_logo[0].href="{repo_url}";
+"""
+
+def setup(app):
+    app.add_js_file(None, body=SET_LOGO_URL_JS)
